58494a20e4d9cd1aeb5faac419919d17675170bb
ducthanh512 <ducthanh512@gmail.com>
2018-07-16 09:55:27 +1000

    update navigation

--- components/Main.js
+++ components/Main.js
@@ -1,24 +1,26 @@
-/*
-*/
-import { Container, Text, Content } from 'native-base';
-import { Icon } from 'native-base';
-import React, { Component } from 'react';
-import { createBottomTabNavigator } from 'react-navigation';
-import { Platform } from 'react-native';
-
-
-import Home from './Home';
-import SearchFragment from './SearchFragment';
-import SearchContainer from './../containers/SearchContainer';
-import NavigatorPage from './NavigatorPage';
-import SearchNavigator from './SearchNavigator';
-
 /**
  * Duc Thanh Nguyen
  * Music Application
  * https://github.com/ducthanh512/MusicAPP.git
  * @flow
  */
+import { Container, Text, Content, } from 'native-base';
+import { Icon } from 'native-base';
+import React, { Component } from 'react';
+import { createBottomTabNavigator, StackNavigator, createStackNavigator } from 'react-navigation';
+import { Platform, StatusBar } from 'react-native';
+
+
+import Home from './Home';
+import SearchFragment from './SearchFragment';
+import SearchContainer from './../containers/SearchContainer';
+
+import GroupContainer from '../containers/GroupContainer';
+import SongListContainer from '../containers/SongListContainer';
+import AudioPlayerFragement from './../components/audio/AudioPlayerFragement';
+import { HOMESCREEN, SONGLISTSCREEN, PLAYLISTLAYOUTSCREEN, AUDIOPLAYERSCREEN } from './../constants/actionType';
+
+
 
 import { YellowBox } from 'react-native';
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated']);
@@ -27,53 +29,92 @@ YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated']);
 //console.disableYellowBox = true; 
 
 export default class Main extends Component {
-    static navigationOptions = {
-        header: null
-        // headerLeft: <Icon
-        //     name={Platform.OS === 'ios' ? 'ios-basket' : 'md-basket'}
-        //     style={{ paddingLeft: 10 }} />,
-        // title: "NativeBase Example",
-        // headerRight: <Icon
-        //     name={Platform.OS === 'ios' ?
-        //         'ios-compass' : 'md-compass'}
-        //     style={{ paddingRight: 10 }} />
-    }
-    render() {
+  static navigationOptions = {
+    header: null
+  }
+  render() {
 
-        const { navigation } = this.props;
-        console.log(JSON.stringify(this.props.mainNavigation));
-        return <MainNavigator />;
-    }
+    const { navigation } = this.props;
+    console.log(JSON.stringify(this.props.mainNavigation));
+    return <MainNavigator />;
+  }
 }
+
+const HomeNavigator = createStackNavigator({
+  HOMESCREEN: {
+    screen: Home,
+  },
+  GROUPSCREEN: {
+    screen: GroupContainer,
+  },
+  SONGLISTSCREEN: {
+    screen: SongListContainer,
+  },
+  AUDIOPLAYERSCREEN: {
+    screen: AudioPlayerFragement,
+  }
+}
+  ,
+  {
+    style: {
+      paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
+    }
+  }
+
+);
+
+
+const SearchNavigator = createStackNavigator({
+  SEARCHSCREEN: {
+    screen: SearchContainer,
+  },
+  AUDIOPLAYERSCREEN: {
+    screen: AudioPlayerFragement,
+  }
+
+},
+  {
+    style: {
+      paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
+    }
+  }
+
+);
+
 const MainNavigator = createBottomTabNavigator(
 
-    {
-
-        NavigatorPage: {
-            screen: NavigatorPage
-        },
-
-        // NavigatorPage: props =>
-        //   <NavigatorPage
-        //     {...props}
-        //     style={{ borderTopColor: '#605F60' }}
-        //   />,
-
-        Search: {
-            screen: SearchNavigator
-        },
-    }, {
-        animationEnabled: true,
-        swipeEnabled: false,
-        tabBarPosition: 'bottom',
-        tabBarOptions: {
-            showIcon: true,
-            showLabel: true,
-            upperCaseLabel: false,
-            style: {
-                backgroundColor: 'white',
-            },
-            activeTintColor: 'blue',
-            inactiveTintColor: 'gray',
+  {
+    NavigatorPage: {
+      screen: HomeNavigator,
+      navigationOptions: {
+        title: 'Home',
+        tabBarIcon: ({ tintColor }) => {
+          return <Icon name={Platform.OS === 'ios' ? 'ios-home-outline' : 'md-home'} style={{ color: tintColor }}></Icon>
         }
-    });
+      }
+    },
+
+    Search: {
+      screen: SearchNavigator,
+      navigationOptions: {
+        title: 'Search',
+        tabBarIcon: ({ tintColor }) => {
+          return <Icon name={Platform.OS === 'ios' ? 'ios-search' : 'md-search'} style={{ color: tintColor }}></Icon>
+        }
+      }
+    },
+  }, {
+    animationEnabled: true,
+    swipeEnabled: false,
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+      showIcon: true,
+      showLabel: true,
+      upperCaseLabel: false,
+      style: {
+        backgroundColor: 'white',
+      },
+      activeTintColor: 'blue',
+      inactiveTintColor: 'gray',
+    }
+  });
